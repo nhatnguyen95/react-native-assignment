b310f77214b9da3b2fc57cb528a1d92659a85ef6
Nguyen Nguyen <nguyennn@Nguyens-iMac.local>
2020-08-13 17:04:30 +0700

    Integrating unit test for react navigation

--- src/screens/HomeScreen/components/TripItem.test.js
+++ src/screens/HomeScreen/components/TripItem.test.js
@@ -4,10 +4,8 @@ import React from "react";
 import TripItem from "./TripItem";
 
 import renderer from "react-test-renderer";
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-Enzyme.configure({ adapter: new Adapter() });
+import { shallow } from "enzyme";
+import { Text } from "react-native";
 
 describe("TripItem", () => {
   describe("Rendering", () => {
@@ -21,9 +19,18 @@ describe("TripItem", () => {
 describe("Press on TripItem", () => {
   it("should call onPress", () => {
     const mockOnPress = jest.fn();
-    const component = Enzyme.shallow(<TripItem onPress={mockOnPress} />);
-    const onPress = component.prop('onPress');
+    const component = shallow(<TripItem onPress={mockOnPress} />);
+    const onPress = component.prop("onPress");
     onPress();
     expect(mockOnPress).toHaveBeenCalled();
+  });
+});
+
+describe("Passing id to trip", () => {
+  it("should equal 10", () => {
+    const component = shallow(<TripItem id={10} />);
+    const text = component.find(Text);
+    const id = text.props().children[1];
+    expect(id).toBe(10);
   });
 });
